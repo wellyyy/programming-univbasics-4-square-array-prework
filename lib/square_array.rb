467c4cbef513ddd8eb9417c33c1ea51c2ed57b5b
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-07-21 03:01:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0
   while counter < array.length do 
-    array[counter] = array[counter]**2
+    array[counter] = array[counter]**
     counter += 1
   end
   array
